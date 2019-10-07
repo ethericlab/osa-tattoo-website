f8dab9dcbbd83aaa411492108e35afde2f5f645d
Nikita Kurpas <nikitakurpas@gmail.com>
2019-10-07 22:26:14 +0900

    Add theme

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export default {
+  colors: {
+    gray: {
+      light: '#BFBFBF',
+      lighter: '#D9D9D9'
+    },
+    black: {
+      default: '#0A0A0A'
+    },
+    red: {
+      default: '#F30016'
+    },
+  }
+}
